78e9d3bebe9c11bad70084eae31a5f14358ecfef
Jaehyun Shin_Linux <tlswogus30@naver.com>
2022-08-08 21:31:21 +0900

    AWS Amplify 기초

---
+++ amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "serverlessb7b56b1c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
